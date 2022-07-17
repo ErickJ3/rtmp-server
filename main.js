4cf8fc762b777195b9a4c87e6fbd9d9097d6944d
Erick Jesus <erick.jesus2060@gmail.com>
2022-07-17 18:52:53 -0300

    refactor: add port http

--- main.js
+++ main.js
@@ -4,12 +4,16 @@ const NodeMediaServer = require("node-media-server");
 const config = {
   logType: 3,
   rtmp: {
-    port: process.env.PORT ,
+    port: process.env.PORT || 1935,
     chunk_size: 60000,
     gop_cache: true,
     ping: 30,
     ping_timeout: 60,
   },
+  http: {
+    port: 8080,
+    allow_origin: '*'
+},
 };
 
 var nms = new NodeMediaServer(config);
