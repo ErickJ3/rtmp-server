ae9da6d1bb0a3bc7021d29a8734a44c4b97fa197
Erick Jesus <erick.jesus2060@gmail.com>
2022-07-17 18:53:48 -0300

    refactor: port

--- main.js
+++ main.js
@@ -4,14 +4,14 @@ const NodeMediaServer = require("node-media-server");
 const config = {
   logType: 3,
   rtmp: {
-    port: process.env.PORT || 1935,
+    port: 1935,
     chunk_size: 60000,
     gop_cache: true,
     ping: 30,
     ping_timeout: 60,
   },
   http: {
-    port: 8080,
+    port: process.env.PORT,
     allow_origin: '*'
 },
 };
